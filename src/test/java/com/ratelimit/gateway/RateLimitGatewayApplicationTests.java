98f38e0cbf7321f894e677e0c7c2f76f7b25bdef
biasmj016 <mjkim031@gmail.com>
2025-01-06 15:30:22 +0900

    Project init

---
+++ src/test/java/com/ratelimit/gateway/RateLimitGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimit.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
